8a28a57a61f784de5e47bf8f6f4751b1663d654c
Laurent Courty <lrntct@gmail.com>
2020-06-18 08:15:23 -0500

    edit bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,9 @@
+echo "cmake generate build"
 cmake.exe CMakeLists.txt
 if errorlevel 1 exit /b 1
+echo "cmake compile"
 cmake.exe --build . --config Release
 if errorlevel 1 exit /b 1
+echo "install"
 if not exist "%PREFIX%\" mkdir "%PREFIX%"
 COPY bin\swashes.exe %PREFIX%\
