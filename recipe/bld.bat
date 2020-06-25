70559cba330813eaae6145a64c50126ef052faea
Laurent Courty <lrntct@gmail.com>
2020-06-24 20:22:28 -0500

    bld.bat: force nmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,7 @@
-echo "cmake generate build"
+set "CMAKE_GENERATOR=NMake Makefiles"
 cmake.exe CMakeLists.txt
 if errorlevel 1 exit /b 1
-echo "cmake compile"
 cmake.exe --build . --config Release
 if errorlevel 1 exit /b 1
-echo "install"
 if not exist "%PREFIX%\" mkdir "%PREFIX%"
 COPY bin\swashes.exe %PREFIX%\
