103b1437aea53c8ba0e507ff98b978d6d8d1ad17
Laurent Courty <lrntct@gmail.com>
2025-04-25 11:53:32 -0600

    update to 1.5.0

--- recipe/bld.bat
+++
@@ -1,7 +0,0 @@
-set "CMAKE_GENERATOR=NMake Makefiles"
-cmake.exe CMakeLists.txt
-if errorlevel 1 exit /b 1
-cmake.exe --build . --config Release
-if errorlevel 1 exit /b 1
-if not exist "%PREFIX%\" mkdir "%PREFIX%"
-COPY bin\swashes.exe %PREFIX%\
